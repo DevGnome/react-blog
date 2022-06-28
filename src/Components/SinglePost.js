66ad30d4f98199096be322f573839ee7bc780ce4
Hugh <88152309+DevGnome@users.noreply.github.com>
2022-06-28 17:19:11 -0400

    create about page, moved fetch to app

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+
 function SinglePost({ title, body, author }) {
-    function handleDelete() {
-        console.log('delete');
-    }
+   // Need delete
+
+        
     
+
     return (
         <div className="single-post">
             <h1>{title}</h1>
             <h4>Written by {author}</h4>
             <p>{body}</p>
+            {/* <button onClick={handleDeleteClick} className="emoji-button delete">
+                🗑
+            </button> */}
         </div>
     );
 }
