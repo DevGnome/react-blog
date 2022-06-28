6c8df41da9bccb6b2c6cc50d41400d6c186e9c77
Hugh <88152309+DevGnome@users.noreply.github.com>
2022-06-28 19:28:46 -0400

    added useFetch hook

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,3 @@
-// import { Link } from 'react-router-dom';
-
-// const NavBar = () => {
-//     return (
-//         <div className="nav-bar">
-//             <Link to="/">Home</Link>
-//             <Link to="/create-post">New Post</Link>
-//         </div>
-//     );
-// }
-// export default NavBar;
-
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
